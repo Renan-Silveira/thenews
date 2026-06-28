55d23c24be8d567e5b08bca7e241e7859270069a
Renan Silveira <renanesilveira@hotmail.com>
2026-06-28 16:29:41 -0300

    Initial commit

---
+++ clean_a.py
@@ -0,0 +1,7 @@
+def clean_a(df):
+    import pandas as pd
+    for coluna in df.select_dtypes(include="str").columns:
+        df[coluna] = df[coluna].str.upper() #colocando as colunas texto em maiúscula
+        df[coluna] = df[coluna].str.rstrip() #removendo espaços em branco na direita
+        df[coluna] = df[coluna].str.lstrip() #removendo espaços em branco na esquerda
+    return df
