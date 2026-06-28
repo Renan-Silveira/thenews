08f6c2d97a161b00fc75217caff63da6748fdb38
Renan Silveira <renanesilveira@hotmail.com>
2026-06-28 18:22:50 -0300

    Update clean_s.py
    tratamento de de datas e colunas bool

--- modules/clean_s.py
+++ modules/clean_s.py
@@ -5,4 +5,9 @@ def clean_s(df):
         df[coluna] = df[coluna].str.upper() #colocando as colunas texto em maiúscula
         df[coluna] = df[coluna].str.rstrip() #removendo espaços em branco na direita
         df[coluna] = df[coluna].str.lstrip() #removendo espaços em branco na esquerda
+    df['word_date'] = pd.to_datetime(df['word_date'], dayfirst=True, errors='coerce')
+    df['played_next_day'] = df['played_next_day'].astype(bool)
+    df['newsletter_open_before_game'] = df['newsletter_open_before_game'].astype(bool)
+    df['active_d30'] = df['active_d30'].astype(bool)
+    df = df.dropna()
     return df
